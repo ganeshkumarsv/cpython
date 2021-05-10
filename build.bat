99b521451ecf1ce33a0911dd6eacf48e037822f0
Julien Lebot <julien.ar.lebot@gmail.com>
2021-05-10 12:27:07 +0200

    Fix artifact path

--- build.bat
+++ build.bat
@@ -5,11 +5,13 @@ mkdir build-out
 if not exist build-out exit /b 3
 
 set platf=Win32
+set outdir=
 
 if "%TARGET_ARCH%" == "x64" (
     @echo IN x64 BRANCH
     call %VSTUDIO_ROOT%\VC\Auxiliary\Build\vcvars64.bat
     set platf=x64
+    set outdir=amd64
 )
 
 if "%TARGET_ARCH%" == "x86" (
@@ -18,12 +20,12 @@ if "%TARGET_ARCH%" == "x86" (
 )
 
 msbuild .\PCbuild\python.vcxproj /p:Configuration=Release /p:Platform=%platf%
-copy .\PCbuild\*.dll build-out || exit /b 4
-copy .\PCbuild\*.exe build-out || exit /b 5
-copy .\PCbuild\*.exp build-out || exit /b 6
-copy .\PCbuild\*.lib build-out || exit /b 7
-copy .\PCbuild\*.pdb build-out || exit /b 8
-copy .\PCbuild\*.pyd build-out || exit /b 9
+dir .\PCbuild\%outdir%
+copy .\PCbuild\%outdir%\*.dll build-out || exit /b 4
+copy .\PCbuild\%outdir%\*.exe build-out || exit /b 5
+copy .\PCbuild\%outdir%\*.exp build-out || exit /b 6
+copy .\PCbuild\%outdir%\*.lib build-out || exit /b 7
+copy .\PCbuild\%outdir%\*.pdb build-out || exit /b 8
 goto :EOF
 
 :nomntdir
